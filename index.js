8d129519efd3c2e53697ef83b832457ce526720f
Brune <abrune.osteo@gmail.com>
2022-04-05 22:56:36 +0200

    js first test

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let lolEl = document.getElementById('lol');
-lolEl.textContent = "hello world";
+let btn = document.getElementById('button');
+btn.addEventListener('click', function() {
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+  let result = '';
+  for (let i = 0; i < input.length; i++) {
+	if (input[i] === 'a') {
+	  result += '4';
+	} else if (input[i] === 'e') {
+	  result += '3';
+	} else if (input[i] === 'i') {
+	  result += '1';
+	} else if (input[i] === 'o') {
+	  result += '0';
+	} else if (input[i] === 'u') {
+	  result += '|_|';
+	} else {
+	  result += input[i];
+	}
+  }
+  output.innerHTML = result;
+});
